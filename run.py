c7aa991e4fe1cb49ad6b68537706fe25edb183be
nmluci <yryuu@outlook.com>
2021-11-22 11:28:57 +0800

    [#1] hotfix 3

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
-from logging import debug
 from app import zonaCipta_app
 
-app = zonaCipta_app(True)
-# app = zonaCipta_app(False)
+# app = zonaCipta_app(True)
+app = zonaCipta_app(False)
 
 if __name__ == "__main__":
     app.run(debug=True)
